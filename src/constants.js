6d64662658aad99b2e8de909193bb6010f46a4c2
Jonah <jonahfroggatt@gmail.com>
2026-02-23 15:19:09 +0100

    fix: update link to correct form

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,1 @@
-export const SUGGEST_FORM_URL = 'https://forms.gle/YOUR_FORM_ID_HERE';
-// TODO: Replace with actual Google Form URL
+export const SUGGEST_FORM_URL = 'https://forms.gle/WZxBKgrqehBxNoY58';
